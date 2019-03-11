6452f45a7626b703b11574f106951768aa0a6c9f
Matteo <matteo.filopanti@gmail.com>
2019-03-11 21:49:16 +0100

    JS Funz
    Aggiunta elementi per ritorno API

---
+++ App.js
@@ -0,0 +1,9 @@
+﻿const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+var courses_offered = [{id: 21, name: 'Programmazione'}, {id: 28, name:'Sistemi'}]
+app.get('/', (req, res) => res.send('Ciao a Tutti'))
+app.get('/courses', (req, res) => {
+   res.json(courses_offered)
+})
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
